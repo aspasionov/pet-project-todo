af7a84c528b4395f982295262dd94b59c93f06db
Artem Spasionov <54273077+aspasionov@users.noreply.github.com>
2022-07-17 17:57:53 +0300

    Add validation (#2)
    * Add validation
    
    * Add input component
    
    * Update pr after review

--- src/components/Input/styles.jsx
+++ src/components/Input/styles.jsx
@@ -1,7 +1,35 @@
 import styled from 'styled-components';
 
-export default styled.input`
-  border: none;
-  background-color: #fff;
-  padding: 10px 15px;
+export const Error = styled.div`
+  font-size: 12px;
+  color: red;
+  transform: translateY(-18px);
+`;
+
+export const FormItem = styled.div`
+  margin-bottom: 20px;
+  
+  input {
+    width: 100%;
+    height: 35px;
+    padding: 0 20px;
+  }
+  
+  &.error {
+    input {
+      border: 1px solid red;
+      &:focus {
+        outline: 1px solid red
+      }
+    }
+  }
+
+  '& textarea': {
+    width: 100%;
+    height: 100px;
+  }
+  
+  '&:last-child': {
+  margin-bottom: 0;
+}
 `;
--- src/screens/Login/styles.jsx
+++ src/screens/Login/styles.jsx
@@ -13,26 +13,13 @@ export const Title = styled.h1`
   text-align: center;
 `;
 
-export const FormItem = styled.div`
-  margin-bottom: 20px;
-  
-  input {
-    width: 100%;
-    height: 35px;
-    padding: 0 20px;
-  }
-
-  '& textarea': {
-    width: 100%;
-    height: 100px;
-  }
-  
-  '&:last-child': {
-  margin-bottom: 0;
-}
-`;
-
 export const Form = styled.div`
   width: 400px;
   margin: auto;
+`;
+
+export const Error = styled.div`
+  font-size: 12px;
+  color: red;
+  transform: translateY(-18px);
 `;
